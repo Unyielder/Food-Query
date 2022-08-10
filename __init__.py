4e99321bfb3cd4bbeee614c2e67cb94a4d958e63
Mohamed Temacini <mahim.temacini@gmail.com>
2022-08-09 23:48:26 -0400

    Started refactor

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from .views import main
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(main)
+    return app
+
+
+if __name__ == '__main__':
+    create_app()
--- flaskr/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy import create_engine
-
-engine = create_engine(r"sqlite:///C:\Users\Mohamed\PycharmProjects\Meal Prep\instance\Canadian_Foods.db", convert_unicode=True, echo=False)
-
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-
