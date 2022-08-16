8c7e739a72f237df3cdb33f20f89902a016e251a
Mohamed Temacini <mahim.temacini@gmail.com>
2022-08-16 01:38:42 -0400

    Added route to select searched ingredient

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request
+from starlette.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
-import requests
 from utils import *
+import json
+
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
 
@@ -14,9 +15,18 @@ def index(request: Request):
 
 
 @app.post("/query")
-async def search_food_desc(request: Request):
+async def search_food_desc(request: Request) -> RedirectResponse:
     form = await request.form()
-    foods = form.get("foodName")
-    print(foods)
-    foods = fuzzy_search(foods)
+    user_input = form.get("foodName")
+    foods = fuzzy_search(user_input)
+    foods = json.dumps(foods)
+
+    response = RedirectResponse(f'/query/{foods}')
+    response.status_code = 302
+    return response
+
+
+@app.get('/query/{foods}')
+async def select_food_desc(request: Request, foods: str):
+    foods = json.loads(foods)
     return templates.TemplateResponse("foodNames.html", {"request": request, "foods": foods})
--- utils.py
+++ utils.py
@@ -2,13 +2,12 @@ from fuzzywuzzy import process, fuzz
 import requests
 
 
-def fuzzy_search(text):
+def fuzzy_search(text: str):
     res = requests.get("https://food-nutrition.canada.ca/api/canadian-nutrient-file/food/?lang=en&type=json")
     all_foods = res.json()
     res = process.extract(text, [record['food_description'] for record in all_foods], scorer=fuzz.token_sort_ratio, limit=50)
-    print(res)
-    search_results = []
 
+    search_results = []
     for name in res:
         for mapp in all_foods:
             if name[0] == mapp['food_description']:
