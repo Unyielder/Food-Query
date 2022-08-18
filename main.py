017908ec0ff9c9459754615271e5c54b7dbd25a7
Mohamed Temacini <mahim.temacini@gmail.com>
2022-08-18 13:08:31 -0400

    Added route for nutrient groups

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from starlette.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from utils import *
+from food import Food
 import json
 
 
@@ -35,20 +36,38 @@ async def select_food_desc(request: Request, user_input: str):
     form = await request.form()
     food_code = form.get('foodName')
 
-    response = RedirectResponse(f'/query/{user_input}/{food_code}')
+    response = RedirectResponse(f'/query/{food_code}/serving_size')
     response.status_code = 302
     return response
 
 
-@app.get('/query/{user_input}/{food_code}')
-async def get_servings(request: Request, user_input: str, food_code):
+@app.get('/query/{food_code}/serving_size')
+async def get_servings(request: Request, food_code):
     res = requests.get(f'https://food-nutrition.canada.ca/api/canadian-nutrient-file/servingsize/?id={food_code}&type'
                        f'=json&lang=en')
-
     servings = res.json()
     print(servings)
     return templates.TemplateResponse("foodServings.html", {"request": request, "servings": servings})
 
+
+@app.post('/query/{food_code}/serving_size')
+async def select_servings(request: Request, food_code):
+    form = await request.form()
+    serving_size = form.get('ing_measure')
+
+    food = Food(food_code, serving_size)
+    response = RedirectResponse(f'/query/{food_code}/{serving_size}')
+    response.status_code = 302
+    return response
+
+
+@app.get('/query/{food_code}/{serving_size}')
+async def get_nutrient_groups(request: Request, food_code, serving_size):
+    res = requests.get(r"https://food-nutrition.canada.ca/api/canadian-nutrient-file/nutrientgroup/?lang=en&type=json")
+    nutrient_groups = res.json()
+    print(nutrient_groups)
+
+    return templates.TemplateResponse("nutrientGroups.html", {"request": request, "nutrients_groups": nutrient_groups})
 
 
 
