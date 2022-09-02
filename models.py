dacb53427fa2379a42e2aa409c38edf08e92cbf2
Mohamed Temacini <mahim.temacini@gmail.com>
2022-09-01 23:44:56 -0400

    Added unique constraint to id_token and email fields of user doc

--- models.py
+++ models.py
@@ -2,8 +2,8 @@ from mongoengine import Document, StringField
 
 
 class User(Document):
-    id_token = StringField(required=True)
-    email = StringField(required=True)
+    id_token = StringField(required=True, unique=True)
+    email = StringField(required=True, unique=True)
     first_name = StringField(required=True)
     last_name = StringField(required=True)
     profile_url = StringField()
