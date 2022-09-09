2f2185a676f68244ac7db398c56c465a43e08da3
Mohamed Temacini <mahim.temacini@gmail.com>
2022-09-08 22:08:50 -0400

    Moved 'save_to_bookmarks' function into bookmark route

--- routers/bookmark.py
+++ routers/bookmark.py
@@ -15,6 +15,41 @@ async def get_bookmarks(request: Request):
     return templates.TemplateResponse("userBookmarks.html", {"request": request, "bookmarks": bookmarks})
 
 
+@router.post('/query/{food_code}/{food_desc}/{serving_size}', status_code=204)
+async def save_to_bookmarks(request: Request, food_code, food_desc, serving_size):
+    if 'user' in request.session:
+        bookmark = Bookmark.objects(
+            user_id=request.session['id'],
+            food_code=food_code,
+            food_desc=food_desc,
+            serving_size=serving_size
+        ).first()
+
+        if bookmark is None:
+            Bookmark(
+                user_id=request.session['id'],
+                food_code=food_code,
+                food_desc=food_desc,
+                serving_size=serving_size
+            ).save()
+
+        response = RedirectResponse(f'/query/{food_code}/{food_desc}/{serving_size}')
+        response.status_code = 302
+        return response
+
+    else:
+        bookmark_dict = {
+            'food_code': food_code,
+            'food_desc': food_desc,
+            'serving_size': serving_size
+        }
+
+        request.session['bookmark_to_save'] = bookmark_dict
+        response = RedirectResponse(f'/login')
+        response.status_code = 302
+        return response
+
+
 @router.post('/bookmarks/delete/{id_bookmark}', status_code=204)
 async def delete_bookmark(request: Request, id_bookmark):
     bookmark = Bookmark.objects(
--- routers/query.py
+++ routers/query.py
@@ -113,39 +113,3 @@ async def get_nutrients(request: Request, food_code, food_desc, serving_size):
                                                              "df_vita": df_vita,
                                                              "is_bookmarked": is_bookmarked
                                                              })
-
-
-@router.post('/query/{food_code}/{food_desc}/{serving_size}', status_code=204)
-async def save_to_bookmarks(request: Request, food_code, food_desc, serving_size):
-
-    if 'user' in request.session:
-        bookmark = Bookmark.objects(
-            user_id=request.session['id'],
-            food_code=food_code,
-            food_desc=food_desc,
-            serving_size=serving_size
-        ).first()
-
-        if bookmark is None:
-            Bookmark(
-                user_id=request.session['id'],
-                food_code=food_code,
-                food_desc=food_desc,
-                serving_size=serving_size
-            ).save()
-
-        response = RedirectResponse(f'/query/{food_code}/{food_desc}/{serving_size}')
-        response.status_code = 302
-        return response
-
-    else:
-        bookmark_dict = {
-            'food_code': food_code,
-            'food_desc': food_desc,
-            'serving_size': serving_size
-        }
-
-        request.session['bookmark_to_save'] = bookmark_dict
-        response = RedirectResponse(f'/login')
-        response.status_code = 302
-        return response
