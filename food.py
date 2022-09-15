0acf6f0b4b91ada4042c0055682126ce85eb364e
Mohamed Temacini <mahim.temacini@gmail.com>
2022-09-14 23:46:03 -0400

    Made nav responsive and removed food module

--- food.py
+++
@@ -1,6 +0,0 @@
-class Food:
-    def __init__(self, food_code: int, food_desc: str, serving_size: str):
-        self.food_code = food_code
-        self.food_desc = food_desc
-        self.serving_size = serving_size
-
