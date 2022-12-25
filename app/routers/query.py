ab498da0ce48b56d1f659676a58dd7463773c201
Mohamed Temacini <mahim.temacini@gmail.com>
2022-12-25 15:34:29 -0500

    Added try block for if empty dataframe

--- app/routers/query.py
+++ app/routers/query.py
@@ -56,33 +56,36 @@ async def select_servings(food_code, food_desc, ing_measure: str = Form(...)):
 
 @router.get('/query/{food_code}/{food_desc}/{serving_size}')
 async def get_nutrients(request: Request, food_code, food_desc, serving_size):
-    df_amount = await get_food_data(f"https://food-nutrition.canada.ca/api/canadian-nutrient-file/nutrientamount/?id={food_code}&type=json&lang=en")
-    df_amount = df_amount[['food_code', 'nutrient_value', 'nutrient_name_id', 'nutrient_web_name']]
-    df_serving = await get_food_data(f"https://food-nutrition.canada.ca/api/canadian-nutrient-file/servingsize/?id={food_code}&type=json&lang=en")
-    df_serving = df_serving[['food_code', 'conversion_factor_value', 'measure_name']]
-    df_names = await get_food_data(f"https://food-nutrition.canada.ca/api/canadian-nutrient-file/nutrientname/?lang=en&type=json")
-    df_names = df_names[['nutrient_name_id', 'nutrient_web_name', 'unit', 'nutrient_group_id']]
-    df_groups = await get_food_data(f"https://food-nutrition.canada.ca/api/canadian-nutrient-file/nutrientgroup/?lang=en&type=json")
-    df_groups = df_groups[['nutrient_group_id', 'nutrient_group_name']]
+    try:
+        df_amount = await get_food_data(f"https://food-nutrition.canada.ca/api/canadian-nutrient-file/nutrientamount/?id={food_code}&type=json&lang=en")
+        df_amount = df_amount[['food_code', 'nutrient_value', 'nutrient_name_id', 'nutrient_web_name']]
+        df_serving = await get_food_data(f"https://food-nutrition.canada.ca/api/canadian-nutrient-file/servingsize/?id={food_code}&type=json&lang=en")
+        df_serving = df_serving[['food_code', 'conversion_factor_value', 'measure_name']]
+        df_names = await get_food_data(f"https://food-nutrition.canada.ca/api/canadian-nutrient-file/nutrientname/?lang=en&type=json")
+        df_names = df_names[['nutrient_name_id', 'nutrient_web_name', 'unit', 'nutrient_group_id']]
+        df_groups = await get_food_data(f"https://food-nutrition.canada.ca/api/canadian-nutrient-file/nutrientgroup/?lang=en&type=json")
+        df_groups = df_groups[['nutrient_group_id', 'nutrient_group_name']]
 
-    df_food = df_amount.merge(df_names, left_on='nutrient_name_id', right_on='nutrient_name_id')
-    df_food = df_food.merge(df_groups, left_on='nutrient_group_id', right_on='nutrient_group_id')
-    df_food = df_food.merge(df_serving, left_on='food_code', right_on='food_code')
+        df_food = df_amount.merge(df_names, left_on='nutrient_name_id', right_on='nutrient_name_id')
+        df_food = df_food.merge(df_groups, left_on='nutrient_group_id', right_on='nutrient_group_id')
+        df_food = df_food.merge(df_serving, left_on='food_code', right_on='food_code')
 
-    df_food = df_food[df_food['measure_name'] == serving_size]
-    df_food['serving_value'] = df_food.apply(lambda x: round(x['nutrient_value'] * x['conversion_factor_value'], 3), axis=1)
-    df_food.sort_values(by='serving_value', inplace=True, ascending=False)
+        df_food = df_food[df_food['measure_name'] == serving_size]
+        df_food['serving_value'] = df_food.apply(lambda x: round(x['nutrient_value'] * x['conversion_factor_value'], 3), axis=1)
+        df_food.sort_values(by='serving_value', inplace=True, ascending=False)
 
-    df_aminos = df_food[df_food['nutrient_group_name'] == 'Amino Acids'].reset_index(drop=True)
-    df_lipids = df_food[df_food['nutrient_group_name'] == 'Lipids'].reset_index(drop=True)
-    # Converting grams into milligrams in lipids table
-    df_lipids['serving_value'] = [val * 1000 if unit == 'g' else val for val, unit in zip(df_lipids['serving_value'], df_lipids['unit'])]
-    df_lipids['unit'] = ['mg' for i in range(df_lipids.shape[0])]
-    df_minerals = df_food[df_food['nutrient_group_name'] == 'Minerals'].reset_index(drop=True)
-    df_carbs = df_food[df_food['nutrient_group_name'] == 'Other Carbohydrates'].reset_index(drop=True)
-    df_other = df_food[df_food['nutrient_group_name'] == 'Other Components'].reset_index(drop=True)
-    df_prox = df_food[df_food['nutrient_group_name'] == 'Proximates'].reset_index(drop=True)
-    df_vita = df_food[df_food['nutrient_group_name'] == 'Vitamins'].reset_index(drop=True)
+        df_aminos = df_food[df_food['nutrient_group_name'] == 'Amino Acids'].reset_index(drop=True)
+        df_lipids = df_food[df_food['nutrient_group_name'] == 'Lipids'].reset_index(drop=True)
+        # Converting grams into milligrams in lipids table
+        df_lipids['serving_value'] = [val * 1000 if unit == 'g' else val for val, unit in zip(df_lipids['serving_value'], df_lipids['unit'])]
+        df_lipids['unit'] = ['mg' for i in range(df_lipids.shape[0])]
+        df_minerals = df_food[df_food['nutrient_group_name'] == 'Minerals'].reset_index(drop=True)
+        df_carbs = df_food[df_food['nutrient_group_name'] == 'Other Carbohydrates'].reset_index(drop=True)
+        df_other = df_food[df_food['nutrient_group_name'] == 'Other Components'].reset_index(drop=True)
+        df_prox = df_food[df_food['nutrient_group_name'] == 'Proximates'].reset_index(drop=True)
+        df_vita = df_food[df_food['nutrient_group_name'] == 'Vitamins'].reset_index(drop=True)
+    except ValueError:
+        raise HTTPException(404, "Couldn't find specific food data")
 
     is_bookmarked = None
     if 'user' in request.session:
